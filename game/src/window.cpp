4abbe99d83941dbdec68c0645175f26722184c0d
tigranesIIthegreat <tigransuqiasyan.2001@gmail.com>
2024-02-24 18:59:30 +0400

    create the Window class

---
+++ game/src/window.cpp
@@ -0,0 +1,63 @@
+#include "window.hpp"
+
+#include <cassert>
+
+namespace sdl {
+
+size_t Window::_instance_count = 0;
+
+Window::Window(const std::string &title, int width, int height, int flags)
+    : _title(title), _width(width), _height(height), _flags(flags) {
+    if (_instance_count++ == 0) 
+        _initialize_system();
+    set_sdl_window();
+    set_sdl_renderer();
+}
+
+Window::~Window() {
+    SDL_DestroyRenderer(_sdl_renderer);
+    SDL_DestroyWindow(_sdl_window);
+    if (--_instance_count == 0)
+        _deinitialize_system();
+}
+
+void Window::_initialize_system() {
+    if (SDL_InitSubSystem(SDL_INIT_EVERYTHING) < 0) {
+        throw std::runtime_error(std::string(SDL_GetError()));
+    }
+}
+
+void Window::_deinitialize_system() {
+    assert(_instance_count == 0);
+    SDL_Quit();
+}
+
+void Window::set_sdl_window() {
+    _sdl_window = SDL_CreateWindow(_title.data(), _width, _height, _flags);
+    if (_sdl_window == nullptr) {
+        _deinitialize_system();
+        throw std::runtime_error(std::string(SDL_GetError()));
+    }
+}
+
+void Window::set_sdl_renderer() {
+    _sdl_renderer = SDL_CreateRenderer(_sdl_window, nullptr, SDL_RENDERER_ACCELERATED);
+    if (_sdl_renderer) {
+        _deinitialize_system();
+        throw std::runtime_error(std::string(SDL_GetError()));
+    }
+}
+
+const std::string& Window::title() const {
+    return _title;
+}
+
+int Window::width() const noexcept {
+    return _width;
+}
+
+int Window::height() const noexcept {
+    return _height;
+}
+
+} // namespace sdl
---
+++ game/src/window.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <SDL3/SDL.h>
+#include <memory>
+#include <string>
+
+namespace sdl {
+class Window {
+public:
+    Window(const std::string& title, int width, int height, int flags);
+    ~Window();
+
+public:
+    const std::string& title() const;
+    int width() const noexcept;
+    int height() const noexcept;
+
+private:
+    void set_sdl_window();
+    void set_sdl_renderer();
+    static void _initialize_system();
+    static void _deinitialize_system();
+
+private:
+    std::string _title;
+    int _width{};
+    int _height{};
+    int _flags{};
+    SDL_Window* _sdl_window{};
+    SDL_Renderer *_sdl_renderer{};
+
+private:
+    static size_t _instance_count;
+};
+
+} // namespace sdl
