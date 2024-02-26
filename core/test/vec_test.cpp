97f1358ef3a165def645d31188ca5037c5290e52
tigranesIIthegreat <tigransuqiasyan.2001@gmail.com>
2024-02-26 22:48:41 +0400

    minimize vec test file

--- core/test/vec_test.cpp
+++ core/test/vec_test.cpp
@@ -1,30 +1,15 @@
-#include <core/vec.hpp>
 #include <gtest_main.hpp>
+#include <core/vec.hpp>
 
 using namespace core;
 
-TEST(Core, VecAddition) {
+TEST(Core, VecArithmetic) {
     Vec2 vec1{1.1, 2.2};
     Vec2 vec2{3.3, 4.4};
     EXPECT_TRUE((vec1 + vec2).equalsWithEpsilon(Vec2{4.4, 6.6}));
-}
-
-TEST(Engine, VecSubtraction) {
-    Vec2 vec1{1.1, 2.2};
-    Vec2 vec2{3.3, 4.4};
     EXPECT_TRUE((vec1 - vec2).equalsWithEpsilon(Vec2{-2.2, -2.2}));
-}
-
-TEST(Engine, VecMultiplication) {
-    Vec2 vec1{1.1, 2.2};
-    Vec2 vec2{3.3, 4.4};
     EXPECT_TRUE((vec1 * vec2).equalsWithEpsilon(Vec2{3.63, 9.68}));
-}
-
-TEST(Engine, VecDivision) {
-    Vec2 vec1{3.3, 4.4};
-    Vec2 vec2{1.1, 2.2};
-    EXPECT_TRUE((vec1 / vec2).equalsWithEpsilon(Vec2{3, 2}));
+    EXPECT_TRUE((vec2 / vec1).equalsWithEpsilon(Vec2{3, 2}));
 }
 
 TEST(Engine, VecNormalization) {
