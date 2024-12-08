32da62368b5f80673280752b299ac532be3ec4fa
tigranesIIthegreat <tigransuqiasyan.2001@gmail.com>
2024-12-08 20:21:34 +0400

    add basic logging functionality

--- utils/src/log.cpp
+++ utils/src/log.cpp
@@ -1,1 +1,47 @@
 #include "log.hpp"
+
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
+
+Logger& Logger::instance() {
+    static auto instance = Logger();
+    return instance;
+}
+
+void Logger::info(const std::string& message) {
+    log(LoggingLevel::INFO, message);
+}
+
+void Logger::warn(const std::string& message) {
+    log(LoggingLevel::WARN, message);
+}
+
+void Logger::error(const std::string& message) {
+    log(LoggingLevel::ERROR, message);
+}
+
+void Logger::log(LoggingLevel level, const std::string& message) {
+    log_time();
+    static std::string default_setting = "\e[0;37m";
+    static std::string intro[] = {
+        "\e[0;37mINF: ",  // INFO
+        "\e[1;33mWRN: ",  // WARN
+        "\e[1;31mERR: "   // ERROR
+    };
+
+    std::cout << intro[static_cast<size_t>(level)] << message
+              << default_setting << std::endl;
+}
+
+void Logger::log_time() {
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_c = std::chrono::system_clock::to_time_t(now);
+    auto localTime = *std::localtime(&now_c);
+    auto duration = now.time_since_epoch();
+    auto millis = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() % 1000;
+
+    std::cout << std::put_time(&localTime, "[%H:%M:%S") << "."
+              << std::setfill('0') << std::setw(3) << millis << "] ";
+}
--- utils/src/log.hpp
+++ utils/src/log.hpp
@@ -1,1 +1,22 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+
+enum class LoggingLevel { INFO, WARN, ERROR };
+
+class Logger {
+public:
+    static Logger& instance();
+
+    void info(const std::string& message);
+    void warn(const std::string& message);
+    void error(const std::string& message);
+
+private:
+    Logger() = default;
+
+private:
+    void log(LoggingLevel level, const std::string& message);
+    void log_time();
+};
